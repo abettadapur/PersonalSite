3d7f4c339ef85ccca111fdc1569e7c47d7e10a3e
Alex Bettadapur <abettadapur@localhost.localdomain>
2015-07-10 22:10:28 -0400

    Thrid init commit

--- personalsite/app/site/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-from flask import Blueprint
-
-site_blueprint = Blueprint('site', __name__)
-
-from . import views
--- personalsite/manage.py
+++ personalsite/manage.py
@@ -1,19 +1,59 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from flask.ext.script import Manager
+import os
+import json
+import argparse
+import requests
 
-# from werkzeug.contrib.fixers import ProxyFix
+from angular_flask.core import db
+from angular_flask.models import Post
 
-from app import create_app
+
+def create_sample_db_entry(api_endpoint, payload):
+    url = 'http://localhost:5000/' + api_endpoint
+    r = requests.post(
+        url, data=json.dumps(payload),
+        headers={'Content-Type': 'application/json'})
+    print r.text
 
 
-app = create_app()
-# app.wsgi_app = ProxyFix(app.wsgi_app)
-manager = Manager(app)
+def create_db():
+    db.create_all()
 
-@manager.command
-def run():
-    app.run()
 
-if __name__ == "__main__":
-    manager.run()
+def drop_db():
+    db.drop_all()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Manage this Flask application.')
+    parser.add_argument(
+        'command', help='the name of the command you want to run')
+    parser.add_argument(
+        '--seedfile', help='the file with data for seeding the database')
+    args = parser.parse_args()
+
+    if args.command == 'create_db':
+        create_db()
+
+        print "DB created!"
+    elif args.command == 'delete_db':
+        drop_db()
+
+        print "DB deleted!"
+    elif args.command == 'seed_db' and args.seedfile:
+        with open(args.seedfile, 'r') as f:
+            seed_data = json.loads(f.read())
+
+        for item_class in seed_data:
+            items = seed_data[item_class]
+            print items
+            for item in items:
+                print item
+                create_sample_db_entry('api/' + item_class, item)
+
+        print "\nSample data added to database!"
+    else:
+        raise Exception('Invalid command')
+
+if __name__ == '__main__':
+    main()
